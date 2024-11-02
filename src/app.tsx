9228a0705011ac1036156eb5852a1fedb083ad94
Tyler Liu <tyler4long@gmail.com>
2024-11-01 21:22:36 -0700

    First version of crud

--- src/app.tsx
+++ src/app.tsx
@@ -1,24 +1,26 @@
-import { Avatar, Button, Space, Typography } from 'antd';
+import { Avatar, Button, Form, Space, Typography } from 'antd';
 import { auto } from 'manate/react';
 import React from 'react';
 
 import { Store } from './store';
 import { supabase } from './supabase';
 
-const { Title } = Typography;
+const { Title, Text } = Typography;
 
 const App = auto((props: { store: Store }) => {
   const { store } = props;
+  const [title, setTitle] = React.useState('');
+  const [content, setContent] = React.useState('');
   return (
     <>
       <Title>Untitled App</Title>
       <Space>
-        {store.supabaseSession ? (
+        {store.session ? (
           <>
             <Avatar
               src={
                 <img
-                  src={store.supabaseSession.user?.user_metadata.avatar_url}
+                  src={store.session.user?.user_metadata.avatar_url}
                   alt="avatar"
                 />
               }
@@ -48,6 +50,43 @@ const App = auto((props: { store: Store }) => {
           </>
         )}
       </Space>
+      <Title level={2}>Posts</Title>
+      <ul>
+        {store.posts.map((post) => (
+          <li key={post.id}>
+            <Text>{post.title}</Text>
+            <Text>{post.content}</Text>
+            <Button size="small" onClick={() => store.deletePost(post.id)}>
+              Delete
+            </Button>
+          </li>
+        ))}
+      </ul>
+      <Title level={2}>Create Post</Title>
+      <Form labelCol={{ span: 8 }} wrapperCol={{ span: 16 }}>
+        <Form.Item label="Title">
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </Form.Item>
+        <Form.Item label="Content">
+          <textarea
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+          />
+        </Form.Item>
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Button
+            onClick={() => {
+              store.createPost(title, content);
+            }}
+          >
+            Create Post
+          </Button>
+        </Form.Item>
+      </Form>
     </>
   );
 });
--- src/store.ts
+++ src/store.ts
@@ -1,20 +1,119 @@
-import { Session } from '@supabase/supabase-js';
-import { exclude, manage } from 'manate';
+import { RealtimeChannel, Session } from '@supabase/supabase-js';
+import { $, exclude, manage } from 'manate';
 
 import { supabase } from './supabase';
 
+class Post {
+  id: string;
+  title: string;
+  content: string;
+}
+
 export class Store {
-  public count = 0;
-  public supabaseSession: Session | undefined = undefined;
+  session: Session | null = null;
+  posts: Post[] = [];
+  postChannel: RealtimeChannel | undefined;
+
+  async fetchPosts() {
+    const { data, error } = await supabase
+      .from('posts')
+      .select('*')
+      .eq('user_id', this.session?.user?.id);
+    if (error) throw error;
+    this.posts = data.map((post) => post as Post);
+  }
+
+  async updatePost(id: string, title: string, content: string) {
+    const { error } = await supabase
+      .from('posts')
+      .update({ title, content })
+      .eq('id', id);
+    if (error) throw error;
+  }
+
+  async deletePost(id: string) {
+    const { error } = await supabase.from('posts').delete().eq('id', id);
+    if (error) throw error;
+  }
+
+  async createPost(title: string, content: string) {
+    const { error } = await supabase
+      .from('posts')
+      .insert({ title, content, user_id: this.session?.user?.id });
+    if (error) throw error;
+  }
+
+  public subscribe() {
+    const postsChannel = supabase
+      .channel('public:posts')
+      .on(
+        'postgres_changes',
+        { event: '*', schema: 'public', table: 'posts' },
+        (payload) => {
+          const { eventType, new: newPost, old: oldPost } = payload;
+          switch (eventType) {
+            case 'INSERT': {
+              $(store.posts).begin();
+              store.posts.push(newPost as Post);
+              $(store.posts).commit();
+              break;
+            }
+            case 'UPDATE': {
+              $(store.posts).begin();
+              const index = store.posts.findIndex(
+                (post) => post.id === newPost.id,
+              );
+              store.posts[index] = newPost as Post;
+              $(store.posts).commit();
+              break;
+            }
+            case 'DELETE': {
+              $(store.posts).begin();
+              const index = store.posts.findIndex(
+                (post) => post.id === oldPost.id,
+              );
+              store.posts.splice(index, 1);
+              $(store.posts).commit();
+              break;
+            }
+            default: {
+              break;
+            }
+          }
+        },
+      )
+      .subscribe();
+    this.postChannel = exclude(postsChannel);
+  }
+
+  public signIn(session: Session) {
+    this.session = session;
+    this.fetchPosts();
+    this.subscribe();
+  }
+
+  public signOut() {
+    this.session = null;
+    this.posts = [];
+    if (this.postChannel) {
+      this.postChannel.unsubscribe();
+      this.postChannel = undefined;
+    }
+  }
 }
 
 const store = manage(new Store());
 
-supabase.auth.onAuthStateChange((event, session) => {
+supabase.auth.onAuthStateChange(async (event, session) => {
   if (session) {
-    store.supabaseSession = exclude(session);
+    if (!store.session) {
+      // fist time login
+      store.signIn(session);
+    } else {
+      store.session = session;
+    }
   } else {
-    store.supabaseSession = undefined;
+    store.signOut();
   }
 });
 
